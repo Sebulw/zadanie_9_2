315f5685a6ecf278a2fbf32378344c03b088d2b2
Sebastian Wojnicz <sebulw@gmail.com>
2017-09-26 13:49:31 +0200

    script ready to verify

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,16 +1,9 @@
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(9, 22);
-var triangle3Area = getTriangleArea(20, 4);
-function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
-		console.log('Nieprawidłowe dane');
-		alert('Nieprawidłowe dane')
-	}
-	return a*h/2
-};
-console.log(triangle1Area);
-alert(triangle1Area);
-console.log(triangle2Area);
-alert(triangle2Area);
-console.log(triangle3Area);
-alert(triangle3Area)
+var femaleNames = ['Magda', 'Karolina', 'Marta', 'Kamila'];
+	maleNames = ['Maciek', 'Artur', 'Paweł', 'Rafał'];
+	allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1) {
+	var namesQuantity = allNames.push(newName);
+	console.log(namesQuantity)
+}
